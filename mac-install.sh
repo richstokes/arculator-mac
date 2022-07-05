eb4b983efe5476457b2d7c0fb36421d3f2a6e08b
Rich Stokes <rich.stokes@gmail.com>
2022-07-04 21:31:52 -0700

    add env var

--- mac-install.sh
+++ mac-install.sh
@@ -12,7 +12,7 @@ echo "Installing Mac dependencies.."
 brew install sdl2 libx11 gcc automake autoconf cmake libtool hunspell || true 
 # export LIBRARY_PATH="/usr/local/lib:/usr/local/Cellar/sdl2:/usr/local/lib:/usr/local/Cellar/sdl2:/opt/homebrew/lib:$(brew info libx11 | grep /usr | cut -d" " -f1)/lib:$(brew info sdl2 | grep /usr | cut -d" " -f1):/usr/local/include/wx-3.1"
 # export LD_LIBRARY_PATH=/usr/local/lib
-# export LDFLAGS="-lX11"
+export LDFLAGS="-lX11"
 # export C_INCLUDE_PATH=/usr/local/include/wx-3.1
 # export CFLAGS="-I/usr/local/include"
 # export CXXFLAGS="-I/usr/local/include -I/usr/local/include/wx-3.1"
